2bdbee38dd260b2d4dfa7bae79c2b489665dbdb3
Paulo <paulinho_spfc8@hotmail.com>
2021-12-03 10:34:32 -0300

    voltei para branco

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,6 @@ import 'package:buscador_gifs/ui/home_page.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
-    theme: ThemeData(hintColor: Colors.black),
+    theme: ThemeData(hintColor: Colors.white),
   ));
 }
